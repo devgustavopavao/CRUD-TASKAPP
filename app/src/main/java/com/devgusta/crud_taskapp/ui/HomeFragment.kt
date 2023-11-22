0349c2c5bd0d8c3e26219efb466f8e5123dddf2a
devGustavoPavao <gusta.pavao@gmail.com>
2023-11-22 09:41:06 -0300

    Criado a tela de newTask e adicionado a navegção com o component TabLayout.

--- app/src/main/java/com/devgusta/crud_taskapp/ui/HomeFragment.kt
+++ app/src/main/java/com/devgusta/crud_taskapp/ui/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import com.devgusta.crud_taskapp.R
 import com.devgusta.crud_taskapp.databinding.FragmentHomeBinding
+import com.devgusta.crud_taskapp.ui.adapter.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
@@ -19,6 +21,24 @@ class HomeFragment : Fragment() {
 
         _binding =  FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initTab()
+    }
+
+    private fun initTab() {
+      val adapter = ViewPagerAdapter(requireActivity())
+      binding.viewpager.adapter = adapter
+        adapter.addFragment(TodoFragment(),R.string.tab_todo)
+        adapter.addFragment(DoingFragment(),R.string.tab_doing)
+        adapter.addFragment(DoneFragment(),R.string.tab_done)
+
+        binding.viewpager.offscreenPageLimit = adapter.itemCount
+        TabLayoutMediator(binding.tabs,binding.viewpager) { tab, position ->
+            tab.text = getString(adapter.getTitle(position))
+        }.attach()
     }
 
     override fun onDestroyView() {
